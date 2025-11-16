07934b47c5f6cf8988f80a081d34badba2b50ca4
yohanadd <yohannesaddget@gmail.com>
2025-11-15 21:54:54 -0500

    Update const.js

--- const.js
+++ const.js
@@ -1,7 +1,8 @@
 const addressMapping = {
     "Algonix": "algonixtechnogies@gmail.com",
     "RAMTRUCK": "dispatch@ramtruckingroup.com",
-    "Sandi": "algonixtechnogies@gmail.com"
+    "Sandi": "algonixtechnogies@gmail.com",
+    "D_pfle":"rayxaddisu@gmail.com"
 }
 
 module.exports = { addressMapping };
